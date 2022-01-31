523ef7dead78249e40cb89638c41151a0bbc77fb
Луцевич Данило Олександрович <Luce_dp7x>
2022-01-31 15:28:39 +0200

    Basics

---
+++ C HASHTAG (BASICS)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_HASHTAG__BASICS_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
