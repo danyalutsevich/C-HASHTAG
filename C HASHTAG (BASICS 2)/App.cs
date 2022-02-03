8d983336ac3eaca2c3f70339fe6150a867cb9e29
danyalutsevich <luchevich31@gmail.com>
2022-02-03 13:20:14 +0200

    collections

--- C HASHTAG (BASICS 2)/App.cs
+++ C HASHTAG (BASICS 2)/App.cs
@@ -9,7 +9,54 @@ namespace C_HASHTAG__BASICS_2_
         int[] arr;
         int[,] arr2;
         int[][] arr3;
+
         public void Run()
+        {
+
+            CollectionsDemo();
+
+        }
+        public void CollectionsDemo()
+        {
+            List<string> strings;
+            Dictionary<string, string> dict;
+
+
+            strings = new List<string>();
+            strings.Add("first");
+            strings.Add("second");
+            strings.Add("third");
+            foreach (var item in strings)
+            {
+                Console.WriteLine(item);
+            }
+
+            dict = new Dictionary<string, string>();
+
+            dict.Add("1", "first");
+            dict.Add("2", "second");
+            dict.Add("3", "third");
+            dict.Add("7", "third");
+            dict.Add("6", "third");
+            dict.Add("5", "third");
+            dict.Add("4", "third");
+
+            foreach (var item in dict)
+            {
+                Console.WriteLine(item.Key+" "+item.Value);
+            }
+
+
+        }
+        public void CtorDemo()
+        {
+
+            TheClass obj = new TheClass(20);
+            Console.WriteLine(obj);
+
+
+        }
+        public void ArraysDemo()
         {
             Console.WriteLine("Hi universe!");
             arr = new int[10];
@@ -28,9 +75,6 @@ namespace C_HASHTAG__BASICS_2_
                 for (int j = 0; j < 4; j++)
                 {
                     arr2[i, j] = 10 * i + j * 11;
-
-
-
                 }
             }
             for (int i = 0; i < 4; i++)
@@ -38,9 +82,6 @@ namespace C_HASHTAG__BASICS_2_
                 for (int j = 0; j < 4; j++)
                 {
                     Console.Write(arr2[i,j]+" ");
-
-
-
                 }
                 Console.WriteLine();
             }
@@ -78,5 +119,32 @@ namespace C_HASHTAG__BASICS_2_
 
 
 
+   }
+   
+    class TheClass
+    {
+        private int x;
+        public int Y { get; set; } //auto property
+        public int W { get;private set; } //auto property
+        public int X { 
+            get { return this.x; } 
+            set { this.x = value; } 
+        }
+        private TheClass()//default ctor
+        {
+            x = 10;
+        }
+        public TheClass(int x)//parametr ctor
+        {
+            this.x = x;
+            
+        }
+        public override string ToString()
+        {
+            return $"x = {this.x}";
+        }
+
+
     }
+
 }
