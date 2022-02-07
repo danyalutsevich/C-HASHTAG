4a347b885e84339901dc6a7964c0d529fd50df96
danyalutsevich <luchevich31@gmail.com>
2022-02-07 22:10:28 +0200

    WOM

---
+++ WOM/Characters/Character.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WOM.Characters
+{
+    internal class Character
+    {
+
+        public int HP { get; set; }
+        public int Atack { get; set; }
+        public int Defence { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("HP: {0}, A: {1}, D: {2}",HP,Atack,Defence);
+        }
+
+        public static int operator *(Character c1,Character c2)
+        {
+
+            int damage1 = c1.Atack - c2.Defence;
+            if (damage1>0)
+            {
+                c1.HP -=damage1;
+
+            }
+            else
+            {
+                c1.HP--;
+            }
+
+            int damage2 = c2.Atack - c1.Defence;
+            if (damage2 > 0)
+            {
+
+                c2.HP-=damage2;
+
+            }
+            else
+            {
+                c2.HP--;
+            }
+
+
+            return damage1+damage2; 
+        } 
+
+       
+
+    }
+}
---
+++ WOM/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace WOM
+{   
+    // resources for all project
+    class Services
+    {
+
+        public static Random random = new Random();
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var world = new World();
+            //world.Go();
+            
+            int i1 = Services.random.Next(5);
+            int i2;
+            do
+            {
+                i2 = Services.random.Next(5);
+
+            } while (i1 == i2);
+
+            int res=world[i1] * world[i2];
+
+            Console.WriteLine(i1.ToString()+world[i1]);
+            Console.WriteLine(i2.ToString()+world[i2]);
+
+
+        }
+    }
+}
