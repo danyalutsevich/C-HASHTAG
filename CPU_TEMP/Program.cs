985536f6f77ef6aa013013a0ea20efc3b9d2d928
danyalutsevich <luchevich31@gmail.com>
2022-02-14 23:43:38 +0200

    Temperature Enum

---
+++ CPU_TEMP/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace CPU_TEMP
+{
+    using Temp;
+    internal class Program
+    {
+        public static Random random = new();
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(RandomTemp());
+
+        }
+
+        public static Temp RandomTemp()
+        {
+
+            Temp[] t =(Temp[])Enum.GetValues(typeof(Temp));
+
+            int temperature = random.Next((int)Temp.Cold,(int)Temp.Hot);
+            Console.WriteLine(temperature);
+
+            if (temperature < (int)Temp.Warm)
+            {
+                return Temp.Cold;
+            }
+            else if(temperature < (int)Temp.Hot)
+            {
+                return Temp.Warm;
+            }
+            else
+            {
+                return Temp.Hot;
+            }
+
+        }
+
+    }
+}
---
+++ CPU_TEMP/temp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temp
+{
+    public enum Temp
+    {
+        Cold=10,
+        Warm=40,
+        Hot=70
+    }
+
+    
+
+}
+
