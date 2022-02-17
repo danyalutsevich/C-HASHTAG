56554ab7649ced7c4edfbaef3574c87e8b7175ef
danyalutsevich <luchevich31@gmail.com>
2022-02-17 22:07:04 +0200

    file Reader

---
+++ IO/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+
+namespace IO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const string path = "readThisFile.txt";
+            string content=String.Empty;
+            try
+            {
+
+                using (StreamReader reader = new StreamReader(path))
+                {
+
+                    content = reader.ReadToEnd();
+                    Console.WriteLine(content);
+                  
+                }// тут будет вызват Dispose автоматически
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+
+            //writer.Flush();   // скинуть буфер наа диск, файл остается открытым
+
+
+        }
+    }
+}
