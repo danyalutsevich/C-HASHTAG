6ac0678126e767f12508a6d31bf86546c811947d
danyalutsevich <luchevich31@gmail.com>
2022-02-25 14:14:22 +0200

    IoC containers

--- IO/IoC.cs
+++ IO/IoC.cs
@@ -7,6 +7,12 @@ using Microsoft.Practices.Unity;
 
 namespace IO
 {
+
+    class Greeter
+    {
+        public string Hello { get => "hello"; }
+    }
+
     internal class IoC
     {
         public static UnityContainer container;
@@ -15,13 +21,16 @@ namespace IO
         {
             container = new UnityContainer();
             container.RegisterInstance<Random>(new Random());
-
+            container.RegisterType<Greeter>();
 
             var rndInt = container.Resolve<RandomInt>();
             rndInt.Print();
 
             var rndDouble = container.Resolve<RandomDouble>();
             rndDouble.Print();
+
+            var rndChar = container.Resolve<RandomChar>();
+            rndChar.Print();
 
         }
     }
@@ -35,20 +44,36 @@ namespace IO
         {
             Console.WriteLine(rnd.Next());
         }
-
-
     }
 
     class RandomDouble
     {
-
         [Dependency]
         public Random rnd { get; set; }
+        [Dependency]
+        public Greeter greeter { get; set; }
+
         public void Print()
         {
-            Console.WriteLine(rnd.NextDouble());
+            Console.WriteLine($"{greeter.Hello} {rnd.NextDouble()}");
         }
+    }
 
+    class RandomChar
+    {
+        private  readonly Greeter _greeter;
+
+        private readonly Random _random;
+
+        public RandomChar(Greeter greeter,Random random)
+        {
+            _greeter = greeter;
+            _random = random;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"{_greeter.Hello} {(char)_random.Next(32,128)}");
+        }
 
     }
 
