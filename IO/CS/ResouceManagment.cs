c4fd8f2ca3ef85c2f29bb61f4ba4e9bf3cad8362
danyalutsevich <luchevich31@gmail.com>
2022-03-16 20:45:22 +0200

    xml

---
+++ IO/CS/ResouceManagment.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IO
+{
+    internal class ResouceManagment
+    {
+        public static void Main()
+        {
+            Disposable disposable = null;
+            try
+            {
+                disposable = new Disposable();
+                disposable.Dispose();
+            }
+            catch
+            {
+                Console.WriteLine("Create err");
+            }
+            finally
+            {
+                disposable?.Dispose();
+            }
+
+            using (var dc = new Disposable())
+            {
+                Console.WriteLine("using");
+            }
+
+
+        }
+
+        public static void GCDemo()
+        {
+            for(int i = 0; i < 1000000; i++)
+            {
+                new Name();
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+
+    }
+
+    class Name
+    {
+
+        public string[] names { get; set; }
+        public Name()
+        {
+            Console.WriteLine("ctor");
+            names = new string[10000000];
+        }
+
+        ~Name()
+        {
+            Console.WriteLine("finalizer");
+            GC.SuppressFinalize(this);
+        }
+
+    }
+
+    class Disposable : IDisposable
+    {
+        public bool disposed { get; set; }
+        public Disposable()
+        {
+            Console.WriteLine("Disposable class created");
+            disposed = false;
+        }
+
+        public void Dispose()
+        {
+            if (!disposed)
+            {
+                Console.WriteLine("Disposable class disposed");
+                disposed=true;
+            }
+        }
+
+        ~Disposable()
+        {
+            Console.WriteLine("Disposable class finalizer");
+            if (!disposed)
+            {
+                Console.WriteLine("Disposable class disposed");
+                disposed = true;
+            }
+        }
+
+    }
+
+}
---
+++ IO/XML.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace IO
+{
+    public class Item
+    {
+        public string Title { get; set; }
+        public float Price { get; set; }
+        public float Sale { get; set; }
+
+    }
+
+    internal class XML
+    {
+
+        public static void Main()
+        {
+            Random random = new Random();
+
+            Item[] items = new Item[5];
+            items[0] = new Item() { Title = "title", Price = random.Next(1000), Sale = (float)random.NextDouble() * 10 };
+            items[1] = new Item() { Title = "title", Price = random.Next(1000), Sale = (float)random.NextDouble() * 10 };
+            items[2] = new Item() { Title = "title", Price = random.Next(1000), Sale = (float)random.NextDouble() * 10 };
+            items[3] = new Item() { Title = "title", Price = random.Next(1000), Sale = (float)random.NextDouble() * 10 };
+            items[4] = new Item() { Title = "title", Price = random.Next(1000), Sale = (float)random.NextDouble() * 10 };
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Item[]));
+
+
+            using (var sr = new StreamWriter("xml.xml"))
+            {
+                serializer.Serialize(sr, items);
+            }
+
+
+
+
+        }
+
+
+
+    }
+}
