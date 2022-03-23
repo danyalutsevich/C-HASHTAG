c5e5cfa1e12c08e1487bafccf048a3869de84bf8
danyalutsevich <luchevich31@gmail.com>
2022-03-24 01:05:32 +0200

    async

---
+++ IO/CS/Tasks, Threads/FileContent.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using System.Diagnostics;
+
+
+namespace IO
+{
+    internal class FileContent
+    {
+        #region async
+
+        
+
+        #endregion
+        static async Task Main()
+        {
+            #region 
+
+            //var t = Task.Run(() => GetFileContentAsync(@"IO.exe"));
+            //Console.WriteLine(t.Result);
+
+            //Console.WriteLine(GetFileContentAsync2(@"C:\Users\luche\source\repos\C#\IO\IO\.txt").Result);
+            #endregion
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            var r = GetFileContentAsync4("IO.exe");
+            var r1 = GetFileContentAsync4("IO.exe");
+            var r2 = GetFileContentAsync4("IO.exe");
+            //Console.WriteLine(await r);
+
+            Console.WriteLine(await r);
+            Console.WriteLine(await r1);
+            Console.WriteLine(await r2);
+
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+        }
+
+        public static async Task<string> GetFileContentAsync4(string fileName)
+        {
+            string content;
+            using (var sr = new StreamReader(fileName))
+            {
+                content = await sr.ReadToEndAsync();
+            }
+            Thread.Sleep(1000);
+            return content;
+        }
+        
+        public static async Task<string> GetFileContentAsync3(string fileName)
+        {
+
+            string content;
+            using (var sr = new StreamReader(fileName))
+            {
+                content = sr.ReadToEnd();
+            }
+            return content;
+
+        }
+
+        public static string GetFileContentAsync(string fileName)
+        {
+            string content;
+            using(var sr = new StreamReader(fileName))
+            {
+                content = sr.ReadToEnd();
+            }
+            return content;
+        }
+
+        public static Task<string> GetFileContentAsync2(string fileName)
+        {
+
+            return Task.Run(()=> GetFileContentAsync(fileName));
+
+        }
+
+
+    }
+}
--- IO/Sum.cs
+++ IO/Sum.cs
@@ -10,8 +10,50 @@ namespace IO
 {
     internal class Sum
     {
+
+
+        #region Task return
+
+
+        public static void TaskProcStart()
+        {
+            var taskR = Task.Run(TaskProcString);
+            taskR.Wait();
+
+            Console.WriteLine($"res: {taskR.Result}");
+        }
+
+        public static string TaskProcString()
+        {
+            Console.WriteLine("TaskProcS w");
+            return "TaskProcS r";
+        }
+
+        #endregion  
+
+        #region Delegate (не поддерживается современными платформами)
+        delegate void VoidDelegate();
+        public static void DelegateProcStart()
+        {
+            VoidDelegate d = DelegateProc;
+            var action = d.BeginInvoke(null, null);
+            Thread.Sleep(1000);
+            d.EndInvoke(action);
+        }
+
+        public static void DelegateProc()
+        {
+            Console.WriteLine("DelegateProc");
+        }
+
+
+        #endregion
+
+        #region Sum
+        public static readonly object Lock = new object();
+
         public static uint sum { get; set; }
-        static void Main()
+        static void SumStart()
         {
             sum = 0;
 
@@ -52,9 +94,69 @@ namespace IO
 
         public static void IncreaseSum(object num)
         {
-            sum += (uint)num;
+            lock (Lock)
+            {
+                sum += (uint)num;
+            }
+        }
+        #endregion
+
+        #region Async Sum
+
+        public static async Task StartSum()
+        {
+            uint X = 0;
+
+            do
+            {
+                try
+                {
+                    X = Convert.ToUInt32(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            } while (X == 0);
+
+            uint sum = 0;
+
+            List<Task<uint>> tasks = new List<Task<uint>>();
+            
+            //ThreadPool.SetMinThreads(2000,10);
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start(); 
+
+            for (uint i = 1; i <= X; i++)
+            {
+                tasks.Add(SumAsync(i));
+            }
+
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+            for (int i = 0; i < tasks.Count; i++)
+            {
+                sum += await tasks[i];
+            }
+            stopwatch.Stop();
+
+            Console.WriteLine($"Sum: {sum}");
+            Console.WriteLine($"Done in {stopwatch.ElapsedMilliseconds} ms");
+
         }
 
+        public static async Task<uint> SumAsync(uint num)
+        {
+            Thread.Sleep(1000);
+            //Task.Delay(1000).Wait();
+            return num;
+        }
+
+
+
+
+        #endregion
 
     }
 }
